aeaeb2b372c381f7a4714e4b33b8b92c0bda5206
KelliMK <kelli.m.knipe@gmail.com>
2018-10-19 13:59:53 -0400

    models built bitcheszC

--- src/main/java/org/wecancodeit/fullstackreviewsite/model/Category.java
+++ src/main/java/org/wecancodeit/fullstackreviewsite/model/Category.java
@@ -7,6 +7,40 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+<<<<<<< Updated upstream
+=======
+@Entity
+>>>>>>> Stashed changes
 public class Category {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String categoryName;
+	
+	@OneToMany(mappedBy="category")
+	private Collection<Review> reviews;
+	
+	public Category() {}
+	
+	public Category(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Collection<Review> getReviews() {
+		return reviews;
+	}
+
+	@Override
+	public String toString() {
+		return categoryName;
+	}
 }
--- src/main/java/org/wecancodeit/fullstackreviewsite/model/Review.java
+++ src/main/java/org/wecancodeit/fullstackreviewsite/model/Review.java
@@ -11,5 +11,45 @@ public class Review {
 	@Id
 	@GeneratedValue
 	private Long id;
+<<<<<<< Updated upstream
 	
+=======
+	private String languageName;
+	private int yearCreated;
+	private String reviewText;
+	
+	@ManyToOne
+	private Category category;
+	
+	public Review() {}
+	
+	public Review(String languageName, int yearCreated, String reviewText, Category category) {
+		this.languageName = languageName;
+		this.yearCreated = yearCreated;
+		this.reviewText = reviewText;
+		this.category = category;
+	}
+
+
+	public Category getCategory() {
+		return category;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getLanguageName() {
+		return languageName;
+	}
+	
+	public String getReviewText() {
+		return reviewText;
+	}
+
+	public int getYearCreated() {
+		return yearCreated;
+	}
+
+>>>>>>> Stashed changes
 }
