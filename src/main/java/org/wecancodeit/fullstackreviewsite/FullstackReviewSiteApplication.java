0157c031347b95c927b651cae7db9ec32beed828
KelliMK <kelli.m.knipe@gmail.com>
2018-10-19 11:19:31 -0400

    hopefully this one works

---
+++ src/main/java/org/wecancodeit/fullstackreviewsite/FullstackReviewSiteApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.fullstackreviewsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackReviewSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackReviewSiteApplication.class, args);
+	}
+}
