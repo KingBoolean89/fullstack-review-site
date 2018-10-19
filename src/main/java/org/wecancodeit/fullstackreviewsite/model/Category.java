ef80e3020e96b67cfd88b0ffcfbcb61a574ccd1b
KelliMK <kelli.m.knipe@gmail.com>
2018-10-19 13:36:27 -0400

    Remove bin files

--- src/main/java/org/wecancodeit/fullstackreviewsite/model/Category.java
+++ src/main/java/org/wecancodeit/fullstackreviewsite/model/Category.java
@@ -1,5 +1,12 @@
 package org.wecancodeit.fullstackreviewsite.model;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 public class Category {
 
 }
--- src/main/java/org/wecancodeit/fullstackreviewsite/model/Review.java
+++ src/main/java/org/wecancodeit/fullstackreviewsite/model/Review.java
@@ -1,5 +1,15 @@
 package org.wecancodeit.fullstackreviewsite.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Review {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	
 }
